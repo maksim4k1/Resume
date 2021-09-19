d4c79c64541255ee668e643eff693fbe7fd2fbfd
Maksim Bazhenov <maksim4k1@gmail.com>
2021-09-19 13:13:55 +0600

    created Pages and added App Routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
+import AppRouters from "./components/AppRouters";
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <div className="container" style={{margin: "75px auto"}}></div>
+      <div className="container" style={{margin: "75px auto"}}>
+        <AppRouters/>
+      </div>
     </div>
   );
 }
